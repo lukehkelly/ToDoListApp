4bf46f2d694f11c14ee66cbabe5dfa0f16a87fbf
lukehkelly <lhkelly@unc.edu>
2025-10-20 11:55:26 -0400

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Luke Kelly on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
